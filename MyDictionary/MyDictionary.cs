22ad0545fa164a59cbfe0f54bd97a6f3a9d3c8ed
Efecan <efecanbayat@gmail.com>
2021-03-08 00:52:52 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<_Key,_Value>
+    {
+        _Key[] keys;
+        _Value[] values;
+
+        public MyDictionary()
+        {
+            keys = new _Key[0];
+            values = new _Value[0];
+        }
+
+        public void Add(_Key key,_Value value)
+        {
+            _Key[] tempKeys = keys;
+            keys = new _Key[keys.Length + 1];
+            
+            _Value[] tempValues = values;
+            values = new _Value[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " " + values[i]);
+            }
+            
+        }
+      
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(123, "Efe");
+            myDictionary.Add(456, "Mücahit");
+            myDictionary.Add(789, "Onur");
+
+            myDictionary.List();
+            
+        }
+    }
+}
